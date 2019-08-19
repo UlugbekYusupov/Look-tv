d98ba5862a47b9e17da0d23efd9627a7ad36da44
Mirzoulugbek <bilmadim.uz@gmail.com>
2019-08-19 14:37:43 +0900

    Initial Commit

---
+++ Looktv/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Looktv
+//
+//  Created by Mirzoulugbek Yusupov on 8/19/19.
+//  Copyright © 2019 Mirzoulugbek Yusupov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
